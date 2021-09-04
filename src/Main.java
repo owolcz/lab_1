2521be6b8675ff8db375db678c87547c7733c841
krzys <krzysztofmuller1@gmail.com>
2021-09-05 00:26:34 +0200

    ćw.4

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.5 */
+        /* ćw.4 */
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
-
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
     }
 }
