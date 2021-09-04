5a116bc98c2ef4c0d6e5a76dd25739e1017eccb6
krzys <krzysztofmuller1@gmail.com>
2021-09-04 17:52:23 +0200

    przykład.4

--- src/Main.java
+++ src/Main.java
@@ -53,5 +53,6 @@ public class Main {
         System.out.println("x < y: " + (x < y)); // true
         System.out.println("y >= x: " + (y >= x)); // true
 
+        System.out.println("(x > y) || (a && b): " + ((x > y) || (a && c))); // true
     }
 }
