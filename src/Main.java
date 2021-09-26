b41bc3c36ed8f544558699647220f1e0ddd395be
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-09-26 13:24:37 +0200

    cwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,20 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+
+//        Zad 1.
+
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        int x = 10;
+        int y = 15;
+        int z = 10;
+
+        a && b
+
+
+
     }
 }
