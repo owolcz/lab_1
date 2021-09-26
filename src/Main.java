cd52dc79c8581d9c41abad9482ff82018e6263fe
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-09-26 16:44:45 +0200

    cwiczenie 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
-
+        System.out.println("Przykładowy tekst");
+        System.out.println("Przykładowy tekst");
     }
 }
