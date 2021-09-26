197a5ac9683f2e7d5abf485b650c753d7d1c8d00
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-09-26 11:43:06 +0200

    int

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        int zmienna = 7;
+
+        System.out.println(zmienna);
         System.out.println("Hello World!");
 
     }
