29741964cc2f71bc7025d29fb7589d77ab03139b
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-09-26 16:55:36 +0200

    cwiczenie 5

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        //Zad.1
         System.out.println("Przykładowy tekst");
         System.out.println("Przykładowy tekst");
-    }
+
+        System.out.println("Hello World!");
+        System.out.println("Przykładowy tekst");
+        System.out.println("Pozdrawiam " +  "Wykładowce");
+
+        System.out.printf("Przykład %s komendy %s oraz wypisanie cyfry %s.\n", "użycia", "printf", 1);
+
+        System.out.print("Sama komenda print");
+        System.out.print(" nie");
+        System.out.print(" przeskakuje automatycznie do drugiej lini na końcu.");
+        System.out.print('\n Używamy do tego symbola '\n' " );
 }
