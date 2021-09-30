f59f810e47aa0b3f03b9d9d09bead40753a9fbdd
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-09-30 03:22:27 +0200

    cwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,21 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+//        Zad.1
+//        Sposób a)
+        double[] tablica = new double[5];
+
+        for(int i=0; i< tablica.length; i ++)
+        {
+            tablica[i] = i + 1.5;
+            System.out.println("Kolejne liczby tablicy to: " + tablica[i]);
+
+        }
+
+//        Sposób b)
+        double[] tablica2 = new double[5];
+
+
+
     }
 }
